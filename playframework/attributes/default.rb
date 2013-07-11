9ecb9e9c25ad128a406983aeb24d87df55ec6801
gkn00001@partner.nri.co.jp <gkn00001@partner.nri.co.jp>
2013-07-11 17:10:44 +0900

    Change playframework recipes/attributes.

--- playframework/attributes/default.rb
+++ playframework/attributes/default.rb
@@ -1,5 +1,5 @@
-default[:playframework][:version] = "2.1.1"
-default[:playframework][:install_dir] = "/opt/play"
-default[:playframework][:user] = "ec2-user"
-default[:playframework][:group] = "ec2-user"
+default['playframework']['version'] = "2.1.1"
+default['playframework']['install_dir'] = "/opt/play"
+default['playframework']['user'] = "ec2-user"
+default['playframework']['group'] = "ec2-user"
 
--- playframework/recipes/default.rb
+++ playframework/recipes/default.rb
@@ -1,16 +1,16 @@
 package "zip"
 
-directory "#{node[:playframework][:install_dir]}" do
-    user "#{node[:playframework][:user]}"
-    group "#{node[:playframework][:group]}"
+directory "#{node['playframework']['install_dir']}" do
+    user "#{node['playframework]['user']}"
+    group "#{node['playframework']['group']}"
     mode "0755"
 end
 
-remote_file "#{node[:playframework][:install_dir]}/#{node[:playframework][:version]}.zip" do
-    source "http://downloads.typesafe.com/play/#{node[:playframework][:version]}/play-#{node[:playframework][:version]}.zip"
-    path "#{node[:playframework][:install_dir]}/#{node[:playframework][:version]}.zip"
-    user "#{node[:playframework][:user]}"
-    group "#{node[:playframework][:group]}"
+remote_file "#{node['playframework']['install_dir']}/#{node['playframework']['version']}.zip" do
+    source "http://downloads.typesafe.com/play/#{node['playframework']['version']}/play-#{node['playframework']['version']}.zip"
+    path "#{node['playframework']['install_dir']}/#{node['playframework']['version']}.zip"
+    user "#{node['playframework']['user']}"
+    group "#{node['playframework']['group']}"
     mode "0755"
     notifies :run, "execute[install-play]", :immediately
     action :create_if_missing
@@ -20,10 +20,10 @@ execute "install-play" do
     user "ec2-user"
     cwd node['playframework']['install_dir']
     command <<-EOH
-    sudo unzip #{node[:playframework][:version]}.zip
-    sudo mv play-#{node[:playframework][:version]} #{node[:playframework][:version]}
-    sudo chown -R #{node[:playframework][:user]}:#{node[:playframework][:group]} #{node[:playframework][:version]} 
-    export PATH=$PATH:#{node[:playframework][:install_dir]}/#{node[:playframework][:version]}
+    sudo unzip #{node['playframework']['version']}.zip
+    sudo mv play-#{node['playframework']['version']} #{node['playframework']['version']}
+    sudo chown -R #{node['playframework']['user']}:#{node['playframework']['group']} #{node['playframework']['version']} 
+    export PATH=$PATH:#{node['playframework']['install_dir']}/#{node['playframework']['version']}
     EOH
     action :nothing
 end
